fe4f6944ee2cabda92dfe78a2c88680572f8c337
Adefokun Adeoluwa Israel <116833049+Adecom16@users.noreply.github.com>
2024-02-02 18:24:54 +0100

    Update hardhat.config.js

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -9,7 +9,7 @@ module.exports = {
 
 const URL =
   "https://eth-sepolia.g.alchemy.com/v2/teS0W8W3d-kbFi9yX5bbRbXdjOMnEXXE";
-const KEY = "9533782f0e391620dfb80bae7007204543096ef332c05d46528a6fb79fd7a536";
+const KEY = "your-private-key";
 module.exports = {
   solidity: "0.8.19",
   networks: {
